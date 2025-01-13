86ad684fbdb157d8b743e11b50275a327aed54e3
nabeel.bhatti <tariq.nabeel@systemsltd.com>
2025-01-13 14:45:56 +0500

    process resource internal external

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,21 @@
+package com.mycompany.app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    @DisplayName(value = "first test")
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
